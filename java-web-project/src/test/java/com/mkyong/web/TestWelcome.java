a30eede43364692156016ef421974ceb3418b12c
dvandyke723 <46197494+dvandyke723@users.noreply.github.com>
2019-01-07 14:33:32 -0500

    Update TestWelcome.java

--- java-web-project/src/test/java/com/mkyong/web/TestWelcome.java
+++ java-web-project/src/test/java/com/mkyong/web/TestWelcome.java
@@ -43,7 +43,7 @@ public class TestWelcome {
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(forwardedUrl("/WEB-INF/views/index.jsp"))
-                .andExpect(model().attribute("msg", "Hello Infosys World"));
+                .andExpect(model().attribute("msg", "Hello Infosys"));
 
     }
 
